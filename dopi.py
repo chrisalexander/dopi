9f4d97953470e8db54ca5830946e6912ad3af3e9
Chris Alexander <chris@chris-alexander.co.uk>
2013-12-24 21:00:19 +0000

    Final testing script

--- dopi.py
+++ dopi.py
@@ -3,6 +3,16 @@ import initio, time
 config = initio.InitioConfiguration();
 dopi = initio.Initio(config);
 
+def irCallback(l, r):
+	print "IR change: (%s, %s)" % (str(l), str(r));
+
+dopi.ir.addCallback(irCallback);
+
+def floorCallback(l, r):
+	print "Floor change: (%s, %s)" % (str(l), str(r));
+
+dopi.floor.addCallback(floorCallback);
+
 dopi.drive.forwards();
 
 dopi.head.pan(-100);
@@ -43,6 +53,7 @@ dopi.drive.stop();
 
 time.sleep(3);
 
+print "Ultrasonic"
 print dopi.ultrasonic.query();
 
 time.sleep(3);
